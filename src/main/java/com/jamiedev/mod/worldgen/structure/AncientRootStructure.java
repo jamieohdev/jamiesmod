aac77e234a210642435f97635e7a2127e5ff6971
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-09-13 13:13:54 -0400

    fixed structure and spawn it on underground surfaces

--- src/main/java/com/jamiedev/mod/worldgen/structure/AncientRootStructure.java
+++ src/main/java/com/jamiedev/mod/worldgen/structure/AncientRootStructure.java
@@ -1,65 +1,128 @@
 package com.jamiedev.mod.worldgen.structure;
 
 import com.jamiedev.mod.init.JamiesModStructures;
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.structure.StructureLiquidSettings;
 import net.minecraft.structure.StructurePieceType;
+import net.minecraft.structure.pool.StructurePool;
+import net.minecraft.structure.pool.StructurePoolBasedGenerator;
+import net.minecraft.structure.pool.alias.StructurePoolAliasLookup;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.ChunkRandom;
 import net.minecraft.world.EmptyBlockView;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.HeightContext;
 import net.minecraft.world.gen.chunk.VerticalBlockSample;
 import net.minecraft.world.gen.heightprovider.HeightProvider;
+import net.minecraft.world.gen.structure.DimensionPadding;
+import net.minecraft.world.gen.structure.JigsawStructure;
 import net.minecraft.world.gen.structure.Structure;
 import net.minecraft.world.gen.structure.StructureType;
 
 import java.util.Optional;
 
-public class AncientRootStructure  extends Structure {
-    public static final MapCodec<AncientRootStructure> CODEC = RecordCodecBuilder.mapCodec((instance) -> {
-        return instance.group(configCodecBuilder(instance), HeightProvider.CODEC.fieldOf("height").forGetter((structure) -> {
-            return structure.height;
-        })).apply(instance, AncientRootStructure::new);
-    });
-    public final HeightProvider height;
+public class AncientRootStructure extends Structure {
 
-    public AncientRootStructure(Structure.Config config, HeightProvider height) {
+    // A custom codec that changes the size limit for our code_structure_sky_fan.json's config to not be capped at 7.
+    // With this, we can have a structure with a size limit up to 30 if we want to have extremely long branches of pieces in the structure.
+    public static final MapCodec<AncientRootStructure> CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(AncientRootStructure.configCodecBuilder(instance),
+                    StructurePool.REGISTRY_CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
+                    Identifier.CODEC.optionalFieldOf("start_jigsaw_name").forGetter(structure -> structure.startJigsawName),
+                    Codec.intRange(0, 30).fieldOf("size").forGetter(structure -> structure.size),
+                    HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
+                    Heightmap.Type.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
+                    Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+                    DimensionPadding.CODEC.optionalFieldOf("dimension_padding", JigsawStructure.DEFAULT_DIMENSION_PADDING).forGetter(structure -> structure.dimensionPadding),
+                    StructureLiquidSettings.codec.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter(structure -> structure.liquidSettings)
+            ).apply(instance, AncientRootStructure::new));
+
+    private final RegistryEntry<StructurePool> startPool;
+    private final Optional<Identifier> startJigsawName;
+    private final int size;
+    private final HeightProvider startHeight;
+    private final Optional<Heightmap.Type> projectStartToHeightmap;
+    private final int maxDistanceFromCenter;
+    private final DimensionPadding dimensionPadding;
+    private final StructureLiquidSettings liquidSettings;
+
+    public AncientRootStructure(Structure.Config config,
+                         RegistryEntry<StructurePool> startPool,
+                         Optional<Identifier> startJigsawName,
+                         int size,
+                         HeightProvider startHeight,
+                         Optional<Heightmap.Type> projectStartToHeightmap,
+                         int maxDistanceFromCenter,
+                         DimensionPadding dimensionPadding,
+                         StructureLiquidSettings liquidSettings)
+    {
         super(config);
-        this.height = height;
+        this.startPool = startPool;
+        this.startJigsawName = startJigsawName;
+        this.size = size;
+        this.startHeight = startHeight;
+        this.projectStartToHeightmap = projectStartToHeightmap;
+        this.maxDistanceFromCenter = maxDistanceFromCenter;
+        this.dimensionPadding = dimensionPadding;
+        this.liquidSettings = liquidSettings;
     }
 
-    public Optional<StructurePosition> getStructurePosition(Structure.Context context) {
-        ChunkRandom chunkRandom = context.random();
-        int i = context.chunkPos().getStartX() + chunkRandom.nextInt(16);
-        int j = context.chunkPos().getStartZ() + chunkRandom.nextInt(16);
-        int k = context.chunkGenerator().getSeaLevel();
-        HeightContext heightContext = new HeightContext(context.chunkGenerator(), context.world());
-        int l = this.height.get(chunkRandom, heightContext);
-        VerticalBlockSample verticalBlockSample = context.chunkGenerator().getColumnSample(i, j, context.world(), context.noiseConfig());
-        BlockPos.Mutable mutable = new BlockPos.Mutable(i, l, j);
+    @Override
+    public Optional<Structure.StructurePosition> getStructurePosition(Structure.Context context) {
+        // Turns the chunk coordinates into actual coordinates we can use. (Gets corner of that chunk)
+        ChunkPos chunkPos = context.chunkPos();
 
-        while(l > k) {
-            BlockState blockState = verticalBlockSample.getState(l);
-            --l;
-            BlockState blockState2 = verticalBlockSample.getState(l);
-            if (blockState.isAir() && (blockState2.isOf(Blocks.MOSS_BLOCK) ||
-                    blockState2.isSideSolidFullSquare(EmptyBlockView.INSTANCE, mutable.setY(l),
-                            Direction.UP))) {
-                break;
+        int initialY = this.startHeight.get(context.random(), new HeightContext(context.chunkGenerator(), context.world()));
+        VerticalBlockSample columnOfBlocks = context.chunkGenerator().getColumnSample(chunkPos.getStartX(), chunkPos.getStartZ(), context.world(), context.noiseConfig());
+
+        // Loops until it either finds a valid surface or hits bottom of world.
+        int currentY = initialY;
+        boolean prevBlockWasAir = false;
+        while (currentY > context.chunkGenerator().getMinimumY()) {
+            BlockState currentState = columnOfBlocks.getState(currentY);
+            boolean currBlockWasAir = currentState.isAir();
+
+            if (prevBlockWasAir && !currBlockWasAir) {
+                break; //exit loop. Found a surface with air above. currentY is now a good spot.
             }
+
+            currentY--; // Spot was invalid, move down for next loop.
+            prevBlockWasAir = currBlockWasAir;
         }
 
-        if (l <= k) {
+        // If we hit bottom of world, no valid position was found. Just quit here and return nothing to spawn at the spot.
+        if (currentY <= context.chunkGenerator().getMinimumY()) {
             return Optional.empty();
-        } else {
-            BlockPos blockPos = new BlockPos(i, l, j);
-            return Optional.of(new Structure.StructurePosition(blockPos, (holder) -> {
-                AncientRootGenerator.addPieces(context.structureTemplateManager(), holder, chunkRandom, blockPos);
-            }));
         }
+
+        BlockPos blockPos = new BlockPos(chunkPos.getStartX(), currentY, chunkPos.getStartZ());
+        Optional<StructurePosition> structurePiecesGenerator =
+                StructurePoolBasedGenerator.generate(
+                        context, // Used for StructurePoolBasedGenerator to get all the proper behaviors done.
+                        this.startPool, // The starting pool to use to create the structure layout from
+                        this.startJigsawName, // Can be used to only spawn from one Jigsaw block. But we don't need to worry about this.
+                        this.size, // How deep a branch of pieces can go away from center piece. (5 means branches cannot be longer than 5 pieces from center piece)
+                        blockPos, // Where to spawn the structure.
+                        false, // "useExpansionHack" This is for legacy villages to generate properly. You should keep this false always.
+                        this.projectStartToHeightmap, // Adds the terrain height's y value to the passed in blockpos's y value. (This uses WORLD_SURFACE_WG heightmap which stops at top water too)
+                        // Here, blockpos's y value is 60 which means the structure spawn 60 blocks above terrain height.
+                        // Set this to false for structure to be place only at the passed in blockpos's Y value instead.
+                        // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                        this.maxDistanceFromCenter, // Maximum limit for how far pieces can spawn from center. You cannot set this bigger than 128 or else pieces gets cutoff.
+                        StructurePoolAliasLookup.EMPTY, // Optional thing that allows swapping a template pool with another per structure json instance. We don't need this but see vanilla JigsawStructure class for how to wire it up if you want it.
+                        this.dimensionPadding, // Optional thing to prevent generating too close to the bottom or top of the dimension.
+                        this.liquidSettings); // Optional thing to control whether the structure will be waterlogged when replacing pre-existing water in the world.
+
+        // Return the pieces generator that is now set up so that the game runs it when it needs to create the layout of structure pieces.
+        return structurePiecesGenerator;
     }
 
     public StructureType<?> getType() {
